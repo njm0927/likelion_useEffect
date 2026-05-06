93ef1d6ed2526d2e0f74e39337549d9331764580
Nam Jae Min <njm739@gmail.com>
2026-05-06 19:19:48 +0900

    useState 복습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import NameInput from "./NameInput";
 const App = () => {
-  return <div className="text-red-500 text-7xl">App</div>;
+  return (
+    <div>
+      <NameInput />
+    </div>
+  );
 };
 
 export default App;
