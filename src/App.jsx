9bfa7bd3e07b6b7bf41b09c6c12439a93c7a12c2
Nam Jae Min <njm739@gmail.com>
2026-05-06 20:36:39 +0900

    영화 목록 가져오기

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import NameInput from "./NameInput";
 import HookStructure from "./HookStructure";
 import ItemList from "./ItemList";
 import TimerSwitch from "./TimerSwitch";
+import MovieList from "./MovieList";
 const App = () => {
   return (
     <div>
-      <TimerSwitch />
+      <MovieList />
     </div>
   );
 };
